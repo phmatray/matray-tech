95155baef791afc2630f2a4c6cd47eafd2eab84f
Philippe Matray <phmatray@outlook.com>
2015-09-09 16:59:53 +0200

    remove contact me link add coderdojo link choose your language

---
+++ phmatray-home/app.js
@@ -0,0 +1,44 @@
+﻿'use strict';
+
+var app = angular
+    .module('myApp', ['ngCookies', 'pascalprecht.translate'])
+    .controller('AppCtrl', AppCtrl);
+
+AppCtrl.$inject = ['$translate'];
+
+function AppCtrl($translate) {
+    /*jshint validthis: true */
+    var vm = this;
+
+    vm.useFrench = useFrench;
+    vm.useEnglish = useEnglish;
+    vm.useLeet = useLeet;
+    vm.currentLang = currentLang;
+
+    function useFrench() {
+        $translate.use('fr');
+    }
+
+    function useEnglish() {
+        $translate.use('en');
+    }
+
+    function useLeet() {
+        $translate.use('leet');
+    }
+
+    function currentLang() {
+        return $translate.use();
+    }
+}
+
+app.config(function($translateProvider) {
+    $translateProvider.useStaticFilesLoader({
+        prefix: '/lang/',
+        suffix: '.json'
+    });
+    $translateProvider.useSanitizeValueStrategy(null);
+    $translateProvider.preferredLanguage('fr');
+    $translateProvider.fallbackLanguage('fr');
+    $translateProvider.useLocalStorage();
+});
